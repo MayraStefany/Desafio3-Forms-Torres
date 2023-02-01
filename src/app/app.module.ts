155b833d4ac719148f4eeae8df9fd497ff349d99
Mayra Torres <isemayratorres18@gmail.com>
2023-01-31 23:09:13 -0500

    Se agrego componentes-FormReactivo y Toolbar - Forms avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FormularioReactivoComponent,
+        ToolbarComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ]
 })
 export class AppModule { }
