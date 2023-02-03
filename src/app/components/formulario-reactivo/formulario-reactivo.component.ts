e92ba6086db742893bf706754088d7276a9d4671
Mayra Torres <isemayratorres18@gmail.com>
2023-02-02 23:35:24 -0500

    Se agrego Formularios reactivos & Validaciones - Matricula

--- src/app/components/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/components/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario-reactivo.component.css']
 })
 export class FormularioReactivoComponent {
+  formularioMatricula: FormGroup;
+
+  constructor(){
+    /* VALIDACIONES */
+    let regexCorreo: string='^[a-z]+@[a-z]+\\.[a-z]{2,3}$'
+    let controles: any={
+      curso: new FormControl('',[Validators.required]),
+      alumno: new FormControl('',[Validators.required]),
+      correo: new FormControl('',[Validators.required,Validators.pattern(regexCorreo)]),
+      contraseña: new FormControl('',[Validators.required,Validators.minLength(7)]),
+      matriculado: new FormControl(false,[])
+    }
+    this.formularioMatricula = new FormGroup(controles);
+  }
+
+  matricula(){
+    /* Mensajes de VALIDACIONES */
+    console.log(this.formularioMatricula);
+    if(this.formularioMatricula.controls['correo'].errors?.['pattern']){
+      console.log("Este no es un correo");
+    }
+    if(this.formularioMatricula.controls['correo'].errors?.['required']){
+      console.log("Se necesita ingresar correo");
+    }
+
+    
+  }
 
 }
